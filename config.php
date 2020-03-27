8d83a52aaace4fdcf82882140a155ba9781493bb
pullpy <38297978+pullpy@users.noreply.github.com>
2020-03-27 16:08:59 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?
-/// Setiap login ke web maka confignya berubah.
-/// jangan sering login klo mau confignya awet tanpa capture ulang
-
-$UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36";
-$FaucetPay = "49hlrhbebse7bo2o6fjuifjjst";
-$SessionToken = "5f20d223e3379a3240b902f51e1f6ff88d60a1ae8217e262afadf2266bc105c6";
-$VisitorUnique = "rx12149426.1585299197.9F90E0DE8A364F58F56B953A8954AAFC.1.1.1.1.1.1.1.1.1";
-$RememberMe = "161871%3Af4f4e46c234dc0d0b8b766bb5bee95398e562125c5205a1e73ff02ce4e3a6314%3A3b506221b7ed7c3897f19ce52ca2858515b7f41e3e89afcb717b00a475208d65";
