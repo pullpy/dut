96e85c2a1d7dae579f3323bdf07aa55337aeeaa3
pullpy <38297978+pullpy@users.noreply.github.com>
2020-03-27 15:52:17 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?
+/// Setiap login ke web maka confignya berubah.
+/// jangan sering login klo mau confignya awet tanpa capture ulang
+
+$UserAgent = "xxxxxxxx";
+$FaucetPay = "xxxxxxxx";
+$SessionToken = "xxxxxxxxx";
+$VisitorUnique = "xxxxxxxxx";
+$RememberMe = "xxxxxxxxx";
